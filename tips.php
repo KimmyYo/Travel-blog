86c2da553e782c71bf1c068fb9a1dffcf60ed11d
judilvtrb <118058334+judilvtrb@users.noreply.github.com>
2022-12-11 17:57:40 +0800

    Update tips.php

--- tips.php
+++ tips.php
@@ -38,7 +38,7 @@
                         <div class="sub_menu">
                             <ul class="choices" id="sub_menu_1">
                                 <li class="country_name" id="sub_country_1">
-                                    <a href="us/">
+                                    <a href="us/index.php">
                                         <div>
                                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plane-departure" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
@@ -95,7 +95,7 @@
                                 </ul>
                                 
                                 <li class="country_name" id="sub_country_2">
-                                    <a href="malaysia/">
+                                    <a href="malaysia/index.php">
                                         <div>
                                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plane-departure" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
@@ -151,7 +151,7 @@
                                     </li>
                                 </ul>
                                 <li class="country_name" id="sub_country_3">
-                                    <a href="uk/">
+                                    <a href="uk/index.php">
                                         <div>
                                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plane-departure" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
@@ -348,9 +348,9 @@
                             </div>
                             <div class="sub_item_section" id="secondary_sub_sub_menu">
                                 <ul class="sub_choices" >
-                                    <li class="country_name"><a href="us/">United States</a></li>
-                                    <li class="country_name"><a href="malaysia/">Malaysia</a></li>
-                                    <li class="country_name"><a href="uk/">United Kingdom</a></li>
+                                    <li class="country_name"><a href="us/index.php">United States</a></li>
+                                    <li class="country_name"><a href="malaysia/index.php">Malaysia</a></li>
+                                    <li class="country_name"><a href="uk/index.php">United Kingdom</a></li>
                                     <li class="country_name"><a href="china/ch_index.php">China</a></li>
                                 </ul>
         
@@ -428,9 +428,9 @@
                 <div class="flex">
                     <ul>
                         <li class="title">Destination</li>
-                        <li><a href="us/">United States</a></li>
-                        <li><a href="uk/">United Kingdom</a></li>
-                        <li><a href="malaysia/">Malaysia</a></li>
+                        <li><a href="us/index.php">United States</a></li>
+                        <li><a href="uk/index.php">United Kingdom</a></li>
+                        <li><a href="malaysia/index.php">Malaysia</a></li>
                         <li><a href="china/ch_index.php">China</a></li>
                     </ul>
                 </div>
